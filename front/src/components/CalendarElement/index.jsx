059d640a24a74e42016a2f7ef3d1e564d91e91e5
YudaiShiratori <toritorikebabu1023x@gmail.com>
2020-02-23 15:03:28 +0900

    fix calendar_display

--- front/src/components/CalendarElement/index.jsx
+++ front/src/components/CalendarElement/index.jsx
@@ -2,12 +2,34 @@ import React from "react";
 
 import * as styles from "./style.css";
 
-const CalendarElement = ({ children }) => {
+import { Typography } from "@material-ui/core";
+
+import dayjs from "dayjs";
+
+const CalendarElement = ({ day }) => {
+    const isFirstDay = day.date() === 1;
+    const format = isFirstDay ? "M月D日" : "D";
+    const today = dayjs();
+    const compareFormat = "YYYYMMDD";
+    const isToday = day.format(compareFormat) === today.format(compareFormat);
+    const isCurrentMonth = day.month() === today.month();
+    const textColor = isCurrentMonth ? "textPrimary" : "textSecondary";
+
     return (
         <div className={styles.element}>
-            <div className={styles.date}>{children}</div>
+            <Typography
+                className={styles.date}
+                color={textColor}
+                align="center"
+                variant="caption"
+                component="div"
+            >
+                <span className={isToday ? styles.today : ""}>
+                    {day.format(format)}
+                </span> 
+            </Typography>
         </div>
-    )
-}
+    );
+};
 
 export default CalendarElement;
